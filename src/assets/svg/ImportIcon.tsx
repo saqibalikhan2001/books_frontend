4ecd6625bf9c72dbdb04f8330601607ef518b5b2
Saqib Khan <saqibalikhan2001@gmail.com>
2024-12-18 19:48:49 +0500

    Add files via upload

---
+++ src/assets/svg/ImportIcon.tsx
@@ -0,0 +1,14 @@
+export const ImportIcon = ({ className = "" }) => (
+  <svg
+    className={className}
+    xmlns="http://www.w3.org/2000/svg"
+    width="13.75"
+    height="15"
+    viewBox="0 0 13.75 15"
+  >
+    <path
+      d="M3.75-15V0h12.5V-5.625H15V-1.25H5v-12.5H15v4.375h1.25V-15Zm7.188,4.375-2.7,2.676-.43.449.43.449,2.7,2.676.879-.879L10.2-6.875h7.3v-1.25H10.2l1.621-1.621Z"
+      transform="translate(-3.75 15)"
+    />
+  </svg>
+);
---
+++ src/assets/svg/index.ts
@@ -0,0 +1,13 @@
+export { InfoIcon } from "./InfoIcon";
+export { ImportIcon } from "./ImportIcon";
+export {CreditNoteIcon} from "./CreditNoteIcon";
+export {CreditNoteImage} from "./CreditNoteImage";
+export { PreferencesIcon } from "./PreferencesIcon";
+export { BillIconListing } from "./BillIconListing";
+export {RecordPaymentIcon} from "./recordPaymentIcon";
+export { ContactIconListing } from "./ContactIconListing";
+export { InvoiceIconListing } from "./InvoiceIconListing";
+export { BillFlowDiagram } from "./FlowDiagrams/BillFlowDiagram";
+export { ContactFlowDiagram } from "./FlowDiagrams/ContactFlowDiagram";
+export { InvoiceFlowDiagram } from "./FlowDiagrams/InvoiceFlowDiagram";
+export {SaleOrderFlowDiagram} from "./FlowDiagrams/SaleOrderFlowDiagram";
